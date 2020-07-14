7db1c082ff703b7a88361eb884cfc50eb774445a
jermin12 <jermin.kamel@gmail.com>
2020-07-14 14:34:28 +0800

    Updates

---
+++ precache-manifest.bb7d9c40196458486e71ddf04e8317c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef0a24b8293da8192e7eba0cae9870f",
+    "url": "/Tasks/index.html"
+  },
+  {
+    "revision": "61adaf465741536bde73",
+    "url": "/Tasks/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "61adaf465741536bde73",
+    "url": "/Tasks/static/js/2.2477a34a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tasks/static/js/2.2477a34a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f23bf892c7ab4915db1",
+    "url": "/Tasks/static/js/main.bd93f74f.chunk.js"
+  },
+  {
+    "revision": "da2a325c610011e43732",
+    "url": "/Tasks/static/js/runtime-main.1d35bdef.js"
+  }
+]);
